9a91ac81207c131001ff83b080aeb1dff9c86e44
shaik Asif <smd.20sa@gmail.com>
2023-07-30 03:33:03 +0530

    routerdone

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
+import Cricket from "./Components/pages/Cricket";
+import Finance from "./Components/pages/Finance";
+import Politics from "./Components/pages/Politics";
+import Navbar from "./Components/Navbar/Navbar";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/cricket" element={<Cricket />} />
+            <Route path="/finance" element={<Finance />} />
+            <Route path="/politics" element={<Politics />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
